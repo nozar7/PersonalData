2ac2749507b9c337b524f881bf16538f2bf211c2
rolin7 <rolin457y2300dias@gmail.com>
2022-05-05 09:23:36 -0500

    update code java about personal data

---
+++ DatosPersonales.java
@@ -0,0 +1,7 @@
+public class DatosPersonales {
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Rolin Espinoza");
+		System.out.println("Tengo anios de edad");
+		System.out.println("Mi ciudad natal es Santiago");
+    }
+}
